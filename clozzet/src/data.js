b12ad171a0317f0de1a5af5cb3d79c70bd0c7e3c
Rafsa Asman <aspharnamsat@gmail.com>
2023-07-04 11:35:07 +0300

    created two files under components

---
+++ clozzet/src/data.js
@@ -0,0 +1,35 @@
+export default function data() {
+  const products = [
+    {
+      img: "leather-jacket.png",
+      name: "Brown Leather Jacket",
+      price: "Ksh 2250",
+      description:
+        "fashion women coat leather lapel jacket Medium size casual coat",
+      featured: true,
+    },
+    {
+      img: "sweatshirts.png",
+      name: "Round-neck Sweatshirts",
+      price: "Ksh 2000",
+      description:
+        "Fashion T-shirt Round Neck Solid color Long sleeves autumn winter boutique pullover bottoming sweater  ",
+      featured: true,
+    },
+    {
+      img: "pallazo.png",
+      name: "Pleated Pallazo Trousers",
+      price: "Ksh 1800",
+      description: "Fashion pallazo two pieces vintage baggy trouser",
+      featured: true,
+    },
+    {
+      img: "denim.png",
+      name: "M-22 Denim Jacket  ",
+      price: "Ksh 1250",
+      description:
+        "denim lightweight long sleeve slim casual collar denim jacket",
+      featured: true,
+    },
+  ];
+}
